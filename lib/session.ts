2cab8d337be16cef371b083aac1ffed47b2a4ba9
Joseph Flores <105085111+josephflo@users.noreply.github.com>
2023-10-19 13:47:03 -0300

    feat/google/auth

---
+++ lib/session.ts
@@ -0,0 +1,46 @@
+import { getServerSession } from "next-auth/next";
+import { NextAuthOptions, User } from "next-auth";
+import { AdapterUser } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+import jsonwebtoken from "jsonwebtoken";
+import { JWT } from "next-auth/jwt";
+import { env } from 'node:process'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  // jwt: {
+  //   encode: ({secret, token}) => {
+
+  //   },
+  //   decode: async ({secret,token})=> {
+
+  //   }
+  // },
+  theme: {
+    colorScheme: "light",
+    logo: "/logo.png",
+  },
+  callbacks: {
+    async session({ session }) {
+      return session;
+    },
+    async signIn({ user }: { user: User | AdapterUser }) {
+      try {
+        //get they user if they exist
+
+        //if they dont exist, create them
+
+      return true
+
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+};
